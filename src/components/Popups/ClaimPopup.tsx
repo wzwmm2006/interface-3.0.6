88d1708aae656593e81737598cab0ff6680dc5fd
waynesama <297436994@qq.com>
2023-03-31 16:34:52 +0800

    1

--- src/components/Popups/ClaimPopup.tsx
+++ src/components/Popups/ClaimPopup.tsx
@@ -1,4 +1,4 @@
-import { TokenAmount } from '@uniswap/sdk'
+import { TokenAmount } from 'inswap-uniswap-sdk'
 import React, { useEffect } from 'react'
 import { X } from 'react-feather'
 import styled, { keyframes } from 'styled-components'
