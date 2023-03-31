e7c95b80b8c07e762b60575668940c40b52b8d69
waynesama <297436994@qq.com>
2023-03-31 15:30:45 +0800

    1

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -19,10 +19,8 @@ export function isAddress(value: any): string | false {
 
 const ETHERSCAN_PREFIXES: { [chainId in ChainId]: string } = {
   1: '',
-  3: 'ropsten.',
-  4: 'rinkeby.',
-  5: 'goerli.',
-  42: 'kovan.'
+  280: 'goerli.explorer.zksync.io',
+  324: 'explorer.zksync.io'
 }
 
 export function getEtherscanLink(
@@ -30,7 +28,7 @@ export function getEtherscanLink(
   data: string,
   type: 'transaction' | 'token' | 'address' | 'block'
 ): string {
-  const prefix = `https://${ETHERSCAN_PREFIXES[chainId] || ETHERSCAN_PREFIXES[1]}etherscan.io`
+  const prefix = `https://${ETHERSCAN_PREFIXES[chainId] || ETHERSCAN_PREFIXES[1]}`
 
   switch (type) {
     case 'transaction': {
